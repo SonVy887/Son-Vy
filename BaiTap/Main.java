95699f0fb3cdd51c3c9329381295d9234cb2b994
SonVy887 <vya0346@gmail.com>
2025-10-15 22:47:34 +0700

    Delete BaiTap directory

--- BaiTap/Main.java
+++
@@ -1,9 +0,0 @@
-package BaiTap;
-import java.util.Scanner;
-public class Main {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        StudentService studentService = new StudentService();
-        studentService.Menu(sc);
-    }
-}
